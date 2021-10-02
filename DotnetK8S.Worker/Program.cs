ba05ab30cd359481eb6f18209642d7f5202d2e37
isaevdan <denys.isaiev@synergysports.com>
2021-10-02 12:42:33 +0300

    Initial commit

---
+++ DotnetK8S.Worker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetK8S.Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
